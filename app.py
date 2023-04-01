718b49fe6c451b3fcc4d78e682b939677ca7719f
godot63 <lcalmbach@gmail.com>
2023-04-01 11:10:15 +0200

    Initial v2

--- app.py
+++ app.py
@@ -1,21 +1,75 @@
-from flask import Flask, render_template
-import os
-from datetime import datetime
-from secrets import token_hex
+
+import streamlit as st
+import requests
 import json
-basedir = os.path.abspath(os.path.dirname(__file__))
+import streamlit.components.v1 as components
+from jinja2 import Template
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "secretkey"
+def get_apps():
+    #url = 'https://lc-opendata01.s3.amazonaws.com/apps.json'
+    url = './apps.json'
+    if 'https' in url:
+        response = requests.get(url)
+        result = json.loads(response.text)
+    else:
+        with open(url) as f:
+            result = json.load(f)
+    return result
 
-NEW_BADGE_LIMIT = 30
+def filter_apps(apps):
+    cols = st.columns([1,3])
+    result = apps
 
-@app.route("/", methods=["GET"])
-def home():
-    f = open('apps.json')
-    apps = json.load(f)
+    with cols[0]:
+        form = st.form("🔎 Find")
+        with form:
+            find_expression = st.text_input("🔎 Find")
+            status = st.selectbox('Status',options=['<all>', 'in progress', 'active', 'archived'])
+            if form.form_submit_button("Filter"):
+                if status != '<all>':
+                    result = [obj for obj in apps if obj['status'] == status]
+                else:
+                    result = apps
+
+                if find_expression:
+                    result = [obj for obj in result if find_expression.lower() in (obj['description'] + obj['name']).lower()]
+                    
+    return result
+
+def render_apps(apps):
+    # with open('template.html', 'r') as f:
+    #     template_content = f.read()
+    #     template = Template(template_content)
+    #     html = template.render(apps=filtered_apps)
+    # components.html(html, height=10000)
+    cols = st.columns(3)
+    col=0
     for app in apps:
-        days_since_published = (datetime.today() - datetime.strptime(app['publish-date'],'%Y-%m-%d')).days
-        app['is_new'] = True if days_since_published < NEW_BADGE_LIMIT else False
-    return render_template("home.html", apps=apps)
+        with cols[col]:
+            if 'url' in app:
+                st.markdown(f"### {app['name']}[🚀]({app['url']})")
+            else:
+                st.markdown(f"### {app['name']}")
+            st.markdown(app['description'])
+            st.markdown(f"Status: {app['status']}")
+            st.markdown(f"[git-repo]({app['git']})") 
+        col = col+1 if col < len(cols)-1 else 0
+
+def main():
+    st.set_page_config(
+    page_title="Lukas Apps",
+    page_icon="🤖",
+    layout="wide")
+
+    st.markdown("# Lukas' app collection 💻")
+    apps = get_apps()
+    filtered_apps = filter_apps(apps)
+    st.markdown(f"{len(filtered_apps)} apps found")
+    st.markdown('<br>',unsafe_allow_html=True)
+    render_apps(filtered_apps)
+   
+
+if __name__ == '__main__':
+    main()
+
 
--- start-app.bat
+++
@@ -1,3 +0,0 @@
-set FLASK_ENV = app.py
-set FLASK_ENV=development
-flask run
